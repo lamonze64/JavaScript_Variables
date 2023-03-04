1135ae38ef782a7fc739e0058d9eb138076a49ef
lamonze parker <lamonzesparker@gmail.com>
2023-03-03 22:57:41 -0600

    done with excercise

--- app.js
+++ app.js
@@ -3,8 +3,33 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+const firstName = "Lamonze";
+let lastName = "Parker";
+var age = 51;
 
-// Exercise 2
+lastName = "Gamgee";
+age = 52;
 
-// Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`${language} was created in ${createdYear}`)
+
+let product = "Cool Shirt"
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectSize = "M";
+
+console.log(`${product} in size ${selectSize} is $ ${price} with ${stock} in stock.`);
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log( `${title} by ${author} is ${pageCount} pages long. Your last bookmark page was ${bookmark}.`);  
